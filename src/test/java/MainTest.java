14031a5e4c9fbf53723aead21752599298b231d0
rikster-r <seitdanial43@gmail.com>
2025-09-30 16:24:57 +0500

    Add maven setup

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void emptyTest() {
+        // Just a placeholder test
+        System.out.println("Running empty test...");
+    }
+}
