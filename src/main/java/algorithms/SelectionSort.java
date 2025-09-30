e9a4b112e75b622b2db2d6a71ae626f8c105bdf9
rikster-r <seitdanial43@gmail.com>
2025-09-30 23:24:56 +0500

    Add early termination

--- src/main/java/algorithms/SelectionSort.java
+++ src/main/java/algorithms/SelectionSort.java
@@ -11,15 +11,26 @@ public class SelectionSort {
 
     public static void sort(int[] arr, PerformanceTracker tracker) {
         int n = arr.length;
+        boolean alreadySorted;
+
         for (int i = 0; i < n - 1; i++) {
             int minIdx = i;
+            alreadySorted = true;
+
             for (int j = i + 1; j < n; j++) {
                 tracker.incrementComparisons();
                 tracker.incrementArrayAccesses(2);
+
                 if (arr[j] < arr[minIdx]) {
                     minIdx = j;
                 }
+
+                // Detect not sorted yet
+                if (arr[j] < arr[j - 1]) {
+                    alreadySorted = false;
+                }
             }
+
             if (minIdx != i) {
                 int tmp = arr[minIdx];
                 arr[minIdx] = arr[i];
@@ -27,13 +38,11 @@ public class SelectionSort {
                 tracker.incrementSwaps();
                 tracker.incrementArrayAccesses(4);
             }
-        }
-    }
 
-    public static boolean isSorted(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i - 1] > arr[i]) return false;
+            // Early termination if already sorted
+            if (alreadySorted) {
+                break;
+            }
         }
-        return true;
     }
 }
